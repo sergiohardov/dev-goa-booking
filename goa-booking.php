2b81719de8c0712b50c692f0ba2ac14335287b7e
Sergio <sergiohardov@gmail.com>
2023-03-21 17:53:52 +0200

    Добавлен бутстрап, подключен к странице плагина

--- goa-booking.php
+++ goa-booking.php
@@ -107,7 +107,12 @@ class GoaBooking
 
         if ($screen->id === 'toplevel_page_' . GOA_BOOKING_SLUG_WP_ADMIN_PAGE) {
             // Enqueue styles
+            wp_enqueue_style('goa-booking-bootstrap-icons', GOA_BOOKING_PLUGIN_URL . 'libs/bootstrap/icons/bootstrap-icons.css');
+            wp_enqueue_style('goa-booking-bootstrap', GOA_BOOKING_PLUGIN_URL . 'libs/bootstrap/bootstrap.min.css');
             wp_enqueue_style('goa-booking-admin', GOA_BOOKING_PLUGIN_URL . 'admin/assets/css/style.css');
+
+            // Enqueue scripts
+            wp_enqueue_script('goa-booking-bootstrap', GOA_BOOKING_PLUGIN_URL . 'libs/bootstrap/bootstrap.min.js', '', '', true);
         }
     }
 }
